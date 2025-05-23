b6d167ff5627e11114a841b201b0b14a4f9d1fe9
Greyson-Jun <greyson.jun@gmail.com>
2025-05-23 15:24:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const MENU_ITEMS = {
     "Chilly Dog": 4.00,
     "Italian Beef": 7.00,
     "Polish Sausage": 7.00
+    "SteakCheese": 10.00
 };
 
 const TOPPINGS_LIST = ["Sauerkraut", "Onion", "Fried Onion", "Relish", "Pepper"];
